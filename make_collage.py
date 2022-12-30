3858684f53da82b2a3ac6ae21a671217ae964b2d
JoJoPuppe <marcusloeper@gmx.de>
2022-12-30 14:57:27 +0100

    progress added

--- make_collage.py
+++ make_collage.py
@@ -9,9 +9,14 @@ import pathlib
 from dataclasses import dataclass
 from typing import List
 
+from timeit import default_timer as timer
+from datetime import timedelta
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('csvpath', help='path to csv file')
 parser.add_argument('iter', help='number of iterations per prompt')
+parser.add_argument('stylecnt', help='number of styles')
 args = parser.parse_args()
 
 # prompt,seed,width,height,sampler,cfgs,steps,filename,negative_prompt
@@ -34,26 +39,19 @@ class PromptObject:
     negative_prompt: str
 
 
-def read_csv(csv_path):
+def read_csv(csv_path, style_cnt):
     prompt_obj_list = []
     with open(csv_path, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
         for row in spamreader:
             prompt_obj_list.append(PromptObject(*row))
 
-    return prompt_obj_list[1:3]
-
-def collage_height(obj_list: List[PromptObject]):
-    added_height = 0
-    for obj in obj_list:
-        added_height += int(obj.height)
-
-    return added_height
-
+    return prompt_obj_list[1:style_cnt + 1]
 
 def generate_images(url, promp_obj_list: List[PromptObject], num_iterations=5):
     image_list = []
-    for prompt in promp_obj_list:
+    sum_img = num_iterations * len(promp_obj_list)
+    for idx, prompt in enumerate(promp_obj_list):
         image_row = []
         for iter in range(num_iterations):
 
@@ -61,7 +59,6 @@ def generate_images(url, promp_obj_list: List[PromptObject], num_iterations=5):
                 use_seed = int(prompt.seed) + SEED_DIFFS[iter - 1]
             else:
                 use_seed = int(prompt.seed)
-            print(use_seed)
             payload = {
                 "prompt": prompt.prompt,
                 "steps": int(prompt.steps),
@@ -72,7 +69,13 @@ def generate_images(url, promp_obj_list: List[PromptObject], num_iterations=5):
                 "height": int(prompt.height),
                 "sampler_index": prompt.sampler
             }
+            start = timer()
+
             image_row.append(generate_image(url, payload))
+
+            end = timer()
+            currt_cnt = (idx + 1) * (iter + 1)
+            print(f'{currt_cnt}/{sum_img} - {prompt.prompt[:20]}... - {timedelta(seconds=end-start)}')
         image_list.append(image_row)
     return image_list
 
@@ -86,11 +89,12 @@ def generate_image(url, payload):
 
 
 def resize_all_images(images):
+    MAX_WIDTH = 800
     resized_images = []
     scale_factor = None
     comb_width = sum(i.size[0] for i in images[0])
-    if comb_width > 2000:
-        scale_factor = round(2000 / comb_width, 1)
+    if comb_width > MAX_WIDTH:
+        scale_factor = round(MAX_WIDTH / comb_width, 1)
 
     if scale_factor is None:
         return images
@@ -98,8 +102,8 @@ def resize_all_images(images):
     for row in images:
         new_row = []
         for img in row:
-            n_width = img.size[0] * scale_factor
-            n_height = img.size[1] * scale_factor
+            n_width = round(img.size[0] * scale_factor)
+            n_height = round(img.size[1] * scale_factor)
             new_img = img.resize((n_width, n_height), Image.LANCZOS)
             new_row.append(new_img)
         resized_images.append(new_row)
@@ -107,18 +111,17 @@ def resize_all_images(images):
 
 
 def generate_art_sheet(images):
-    images = resize_all_images(images)
-    height = sum(i.size[1] for i in images[0])
+    height = sum(i[0].size[1] for i in images)
     width = sum(i.size[0] for i in images[0])
     print(f"collage_width = {width}, {height}")
     collage = Image.new("RGBA", (width, height), color=(255,255,255,255))
     for img_row in range(len(images)):
+        y = 0
+        if img_row == 0:
+            y = 0
+        else:
+            y += images[img_row - 1][0].size[1]
         for img_idx in range(len(images[img_row])):
-            print(images[img_row][img_idx].size)
-            if img_row == 0:
-                y = 0
-            else:
-                y = img_row * images[img_row - 1][img_idx].size[1]
             x = img_idx * images[img_row][img_idx].size[0]
             collage.paste(images[img_row][img_idx], (x,y))
 
@@ -143,6 +146,7 @@ def generate_single_art(payload):
         image.save('output.png', pnginfo=pnginfo)
 
 if __name__ == "__main__":
-    promp_obj_list = read_csv(args.csvpath)
+    promp_obj_list = read_csv(args.csvpath, int(args.stylecnt))
     images = generate_images(url, promp_obj_list, num_iterations=int(args.iter))
+    images = resize_all_images(images)
     generate_art_sheet(images)
