d441c24cbef27eb91f06449cb032c8de3482db39
JoJoPuppe <marcusloeper@gmx.de>
2022-12-31 12:39:16 +0100

    added model change

--- make_collage.py
+++ make_collage.py
@@ -8,6 +8,7 @@ import argparse
 import pathlib
 from dataclasses import dataclass
 from typing import List
+from datetime import datetime
 
 from timeit import default_timer as timer
 from datetime import timedelta
@@ -25,7 +26,8 @@ args = parser.parse_args()
 url = "http://127.0.0.1:7860"
 
 SEED_DIFFS = [2, -2, 81, 4, -9, 4816, -4771, 3123, 10233, -10222]
-
+BASEMODEL_STRING = "marcusloeper"
+BASE_OUT_PATH = pathlib.Path("./out")
 
 @dataclass
 class PromptObject:
@@ -38,6 +40,9 @@ class PromptObject:
     steps: str
     filename: str
     negative_prompt: str
+    model_hash: str
+    restore_faces: str
+    denoising_strength: str
 
 
 def read_csv(csv_path, style_cnt):
@@ -49,6 +54,10 @@ def read_csv(csv_path, style_cnt):
 
     return prompt_obj_list[1:style_cnt + 1]
 
+def get_current_model():
+    response = requests.get(url=f'{url}/sdapi/v1/options')
+    json_response = response.json()
+    return json_response['sd_model_checkpoint']
 
 def set_checkpoint_model(url, model_string):
     option_payload = {
@@ -57,28 +66,49 @@ def set_checkpoint_model(url, model_string):
     response = requests.post(url=f'{url}/sdapi/v1/options', json=option_payload)
     return response
 
+
 def get_all_model_names(url):
     response = requests.get(url=f'{url}/sdapi/v1/sd-models')
     # data = response.json()
     return response.json()
 
+def get_model_string(url, name, hash):
+    all_model_names = get_all_model_names(url)
+    all_titles = [i['title'] for i in all_model_names]
+    new_model = None
+    for model in all_model_names:
+        if model['hash'] == hash:
+            new_model = model["title"].replace(BASEMODEL_STRING, name)
+            break
+    if new_model is not None and new_model in all_titles:
+        return new_model
+    elif new_model is not None:
+        for t in all_titles:
+            if t.find(name) != -1:
+                print(f'model not found using: {t}')
+                return t
+    else:
+        raise Exception("no model with {name} found. please train dreambooth!")
 
-def generate_images(url, promp_obj_list: List[PromptObject], num_iterations=5, name=None):
+def generate_images(url, promp_obj_list: List[PromptObject], folder, num_iterations=5, name=None):
     start_all = timer()
     currt_cnt = 1
     image_list = []
     sum_img = num_iterations * len(promp_obj_list)
-    for prompt in promp_obj_list:
+    current_model = get_current_model()
+    for idx, prompt in enumerate(promp_obj_list):
         image_row = []
         for iter in range(num_iterations):
-
             if iter > 0:
                 use_seed = int(prompt.seed) + SEED_DIFFS[iter - 1]
             else:
                 use_seed = int(prompt.seed)
 
             if name is not None:
-                prompt.prompt = prompt.prompt.replace("marcusloeper", name)
+                new_model = get_model_string(url, name, prompt.model_hash)
+                if new_model != current_model:
+                    set_checkpoint_model(url, new_model)
+                prompt.prompt = prompt.prompt.replace(BASEMODEL_STRING, name)
             payload = {
                 "prompt": prompt.prompt,
                 "steps": int(prompt.steps),
@@ -87,11 +117,13 @@ def generate_images(url, promp_obj_list: List[PromptObject], num_iterations=5, n
                 "negative_prompt": prompt.negative_prompt,
                 "width": int(prompt.width),
                 "height": int(prompt.height),
-                "sampler_index": prompt.sampler
+                "sampler_index": prompt.sampler,
+                "denoising_strength": float(prompt.denoising_strength),
+                "restore_faces": prompt.restore_faces
             }
             start = timer()
 
-            image_row.append(generate_image(url, payload))
+            image_row.append(generate_image(url, payload, folder, idx, iter))
 
             end = timer()
             print(f'{currt_cnt}/{sum_img} - {prompt.prompt[:20]}... - {timedelta(seconds=end-start)} - ges: {timedelta(seconds=end - start_all)}')
@@ -100,11 +132,12 @@ def generate_images(url, promp_obj_list: List[PromptObject], num_iterations=5, n
     return image_list
 
 
-def generate_image(url, payload):
+def generate_image(url, payload, folder, idx, iter):
     response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
     r = response.json()
     for i in r['images']:
         image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+        save_image(url, image, i, folder, idx, iter)
         return image
 
 
@@ -130,7 +163,7 @@ def resize_all_images(images):
     return resized_images
 
 
-def generate_art_sheet(images):
+def generate_art_sheet(images, folder):
     height = sum(i[0].size[1] for i in images)
     width = sum(i.size[0] for i in images[0])
     print(f"collage_width = {width}, {height}")
@@ -145,31 +178,38 @@ def generate_art_sheet(images):
             x = img_idx * images[img_row][img_idx].size[0]
             collage.paste(images[img_row][img_idx], (x,y))
 
+    collage_out = pathlib.Path.joinpath(folder, "collage.png")
     collage.show()
-    collage.save("collage.png")
+    collage.save(collage_out)
 
-def generate_single_art(payload):
-    response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
 
-    r = response.json()
+def save_image(url, image, i, folder, idx, iter):
 
-    for i in r['images']:
-        image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+    row_folder = pathlib.Path.joinpath(folder, str(idx))
+    row_folder.mkdir(parents=True, exist_ok=True)
+    png_payload = {
+        "image": "data:image/png;base64," + i
+    }
+    response = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
 
-        png_payload = {
-            "image": "data:image/png;base64," + i
-        }
-        response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
-
-        pnginfo = PngImagePlugin.PngInfo()
-        pnginfo.add_text("parameters", response2.json().get("info"))
-        image.save('output.png', pnginfo=pnginfo)
+    pnginfo = PngImagePlugin.PngInfo()
+    pnginfo.add_text("parameters", response.json().get("info"))
+    out_file_path = pathlib.Path.joinpath(row_folder, f"{iter}-out.png")
+    image.save(out_file_path, pnginfo=pnginfo)
 
 if __name__ == "__main__":
     promp_obj_list = read_csv(args.csvpath, int(args.stylecnt))
     name = None
     if args.name:
         name = args.name
-    images = generate_images(url, promp_obj_list, num_iterations=int(args.iter), name=name)
+    else:
+        raise Exception("please choose a model name.")
+
+    pathlib.Path('/tmp/sub1/sub2').mkdir(parents=True, exist_ok=True)
+    now = datetime.now()
+    folder_name = pathlib.Path.joinpath(BASE_OUT_PATH, f"{name}-{now.strftime('%Y%m%d-%H%M%S')}")
+    folder_name.mkdir(parents=True, exist_ok=True)
+
+    images = generate_images(url, promp_obj_list, folder_name, num_iterations=int(args.iter), name=name)
     images = resize_all_images(images)
-    generate_art_sheet(images)
+    generate_art_sheet(images, folder_name)
